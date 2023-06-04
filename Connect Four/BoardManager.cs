9fd6e8e17124d0ea97a646d2804d79d5383dc7f7
CatsArmy <104517593+CatsArmy@users.noreply.github.com>
2023-06-04 20:43:18 +0300

    ready for submit

--- Connect Four/BoardManager.cs
+++ Connect Four/BoardManager.cs
@@ -65,82 +65,6 @@ namespace Connect_Four
         /// <returns>true if atlesat 1 diagonal is connected</returns>
         private bool CheckDiagonals()
         {
-            /*
-            if (y > 2)
-            {
-                if (x > 2)
-                {
-                    if (board[y - 1, x - 1] == type && board[y - 2, x - 2] == type && board[y - 3, x - 3] == type)
-                    {
-                        return true;
-                    }
-                }
-                if (x < 4)
-                {
-                    if (board[y - 1, x + 1] == type && board[y - 2, x + 2] == type && board[y - 3, x + 3] == type)
-                    {
-                        return true;
-                    }
-                }
-            }
-            if (y < 3)
-            {
-                if (x > 2)// && x< 4)
-                {
-                    if (board[y + 1, x - 1] == type && board[y + 2, x - 2] == type && board[y + 3, x - 3] == type)//line83
-                    {
-                        return true;
-                        //[3,2] == [2,3], [1,4], [0.5]
-                    }
-                }
-                if (x < 4)// && x > 2)
-                {
-                    if (board[y + 1, x - 1] == type && board[y + 2, x - 2] == type && board[y + 3, x - 3] == type)//bad line
-                    {
-                        return true;
-                    }
-                }
-            }
-            if (y < 5 && y > 1)
-            {
-                if (x < 5 && x > 0)
-                {
-                    if (board[y + 1, x - 1] == type && board[y - 1, x + 1] == type && board[y - 2, x + 2] == type)
-                    {
-                        return true;
-                    }
-                }
-                if (x < 6 && x > 1)
-                {
-                    if (board[y + 1, x + 1] == type && board[y - 1, x - 1] == type && board[y - 2, x - 2] == type)
-                    {
-                        return true;
-                    }
-                }
-            }
-            if (y < 4 && y > 0)
-            {
-                if (x < 5 && x > 0)
-                {
-                    if (board[y - 1, x - 1] == type && board[y + 1, x + 1] == type && board[y + 2, x + 2] == type)
-                    {
-                        return true;
-                    }
-                }
-                if (x < 6 && x > 1)
-                {
-                    if (board[y - 1, x + 1] == type && board[y + 1, x - 1] == type && board[y + 2, x - 2] == type)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-            */
-            return Recursion();
-        }
-        bool Recursion()
-        {
             //Counters
             int diagonalSecondary = 0;
             int diagonalPrime = 0;
@@ -181,115 +105,8 @@ namespace Connect_Four
         }
         //y - 1 = x
         //y = 5 == y = bottom
-        bool IsCoordinateValid(int x,int y)
-        {
-            return x >= 0 && y >= 0 && x < 7 && y < 6;
-        }
-        //-1 means out of range | 1 = true | 0 false
-        bool CheckRowUp(int y,int offset)
-        {
-            offset += 1;
-            y -= offset;
-
-            if (!IsCoordinateValid(x, y))
-                return false;
-
-            return board[y, x] == type;
-        }
-        bool CheckRowDown(int y, int offset)
-        {
-            offset += 1;
-            y += offset;
-
-            if (!IsCoordinateValid(x, y))
-                return false;
-
-            return board[y, x] == type;
-        }
-        bool CheckCollumRight(int x,int offset)
-        {
-            offset += 1;
-            x += offset;
-
-            if (!IsCoordinateValid(x, y))
-                return false;
-
-            return board[y, x] == type;
-        }
-        bool CheckCollumLeft(int x, int offset)
-        {
-            offset += 1;
-            x -= offset;
-
-            if (!IsCoordinateValid(x, y))
-                return false;
-
-            return board[y, x] == type;
-        }
-        bool CheckDiagonalUpRight(int x,int y, int offset)
-        {
-            offset += 1;
-            x += offset;
-            y -= offset;
-            if (!IsCoordinateValid(x, y)) 
-                return false;
-
-            return board[y,x] == type;
-        }
-        bool CheckDiagonalDownRight(int x, int y,int offset)
-        {
-            offset += 1;
-            x += offset;
-            y += offset;
-            if (!IsCoordinateValid(x, y))
-                return false;
-
-            return board[y, x] == type;
-        }
-        bool CheckDiagonalUpLeft(int x, int y, int offset)
-        {
-            offset += 1;
-            x -= offset;
-            y -= offset;
-            if (!IsCoordinateValid(x, y))
-                return false;
-
-            return board[y, x] == type;
-        }
-        bool CheckDiagonalDownLeft (int x, int y, int offset)
-        {
-            offset += 1;
-            x -= offset;
-            y += offset;
-            if (!IsCoordinateValid(x, y))
-                return false;
-
-            return board[y, x] == type;
-        }
-
-        /// <summary>
-        /// checks all of the x positions on a certin y level
-        /// </summary>
-        /// <returns>true if atleast 4 x positions of the same type are connected with no split inbetween</returns>
         private bool CheckCollums()
         {
-            /*
-            int numOfCollums = board.GetLength(1);
-            int collumTrackerRight = 0, collumTrackerLeft = 0;
-            for (int i = 0, j = numOfCollums - 1; i < numOfCollums; i++,j--)
-            {
-                if (collumTrackerLeft == 4 || collumTrackerRight == 4)
-                    return true;
-                if (board[y, i] == type)
-                    collumTrackerRight++;
-                else
-                    collumTrackerRight = 0;
-                if (board[y, j] == type)
-                    collumTrackerLeft++;
-                else
-                    collumTrackerLeft = 0;
-            }
-            */
             int collumTracker = 0;
             bool collumRightStop = false;
             bool collumLeftStop = false;
@@ -316,23 +133,6 @@ namespace Connect_Four
         /// <returns>true if atlesat 4 of the same token type is connected</returns>
         private bool CheckRows()
         {
-            /*
-            int numOfRows = board.GetLength(0);
-            int rowTrackerUp = 0, rowTrackerDown = 0;
-            for (int i = 0, j = numOfRows - 1; i < numOfRows; i++, j--) 
-            {
-                if (rowTrackerUp == 4 || rowTrackerDown == 4)
-                    return true;
-                if (board[i, x] == type)
-                    rowTrackerUp++;
-                else
-                    rowTrackerUp = 0;
-                if (board[j, x] == type)
-                    rowTrackerDown++;
-                else
-                    rowTrackerDown = 0;
-            }
-            */
             int rowTracker = 0;
             bool rowUpStop = false;
             bool rowDownStop = false;
@@ -353,6 +153,94 @@ namespace Connect_Four
             }
             return false;
         }
+        bool IsCoordinateValid(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < 7 && y < 6;
+        }
+        bool CheckRowUp(int y, int offset)
+        {
+            offset += 1;
+            y -= offset;
+
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        bool CheckRowDown(int y, int offset)
+        {
+            offset += 1;
+            y += offset;
+
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        bool CheckCollumRight(int x, int offset)
+        {
+            offset += 1;
+            x += offset;
+
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        bool CheckCollumLeft(int x, int offset)
+        {
+            offset += 1;
+            x -= offset;
+
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        bool CheckDiagonalUpRight(int x, int y, int offset)
+        {
+            offset += 1;
+            x += offset;
+            y -= offset;
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        bool CheckDiagonalDownRight(int x, int y, int offset)
+        {
+            offset += 1;
+            x += offset;
+            y += offset;
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        bool CheckDiagonalUpLeft(int x, int y, int offset)
+        {
+            offset += 1;
+            x -= offset;
+            y -= offset;
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        bool CheckDiagonalDownLeft(int x, int y, int offset)
+        {
+            offset += 1;
+            x -= offset;
+            y += offset;
+            if (!IsCoordinateValid(x, y))
+                return false;
+
+            return board[y, x] == type;
+        }
+        /// <summary>
+        /// checks all of the x positions on a certin y level
+        /// </summary>
+        /// <returns>true if atleast 4 x positions of the same type are connected with no split inbetween</returns>
         /// <summary>
         /// uses the x position of where the token was placed to find its y level
         /// </summary>
